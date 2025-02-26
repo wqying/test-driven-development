9f4b4d4cf7a407a60ec5130491b0a38c22be7042
wqying <qywong@uci.edu>
2025-02-26 14:21:33 +0000

    Added first test case

--- src/Experiment.py
+++ src/Experiment.py
@@ -1,17 +1,23 @@
 # Experiment class
-
+from SignalDetection import SignalDetection
 
 class Experiment:
     def __init__(self):
         '''
         Initializes an empty list to store SDT objects and their corresponding labels
         '''
-        self.condition = []
+        empty_list = []
+        self.condition = empty_list
 
     def add_condition(self, sdt_obj: SignalDetection, label: str = None) -> None:
         '''
         Adds a SignalDetection object and an optional label describing the condition to the experiment
         '''
-        self.condition.append((sdt_obj, label))
+        self.condition.append((sdt_obj, label))  # made this into a tuple so that the object and label is connected
 
-    
+    def sorted_roc_points(self) -> tuple[list[float], list[float]]:
+        '''
+        Return sorted false alarm rates and hit rates for plotting the ROC curve
+        '''
+        pass
+         
--- src/TestExperiment.py
+++ src/TestExperiment.py
@@ -1,1 +1,16 @@
 # Unit test for Experiment.py
+import unittest
+from Experiment import Experiment
+
+
+class TestExperiment(unittest.TestCase):
+    def test_init(self):
+        exp = Experiment()
+        self.assertEqual(exp.condition, [])
+
+    
+
+
+
+if __name__ == "__main__":
+    unittest.main()
