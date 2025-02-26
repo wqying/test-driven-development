97334f78f6470525f967791b3cbb462f2de2e9a5
wqying <qywong@uci.edu>
2025-02-26 14:32:52 +0000

    Added test_add_condition test case

--- src/TestExperiment.py
+++ src/TestExperiment.py
@@ -1,6 +1,7 @@
 # Unit test for Experiment.py
 import unittest
 from Experiment import Experiment
+from SignalDetection import SignalDetection
 
 
 class TestExperiment(unittest.TestCase):
@@ -8,7 +9,11 @@ class TestExperiment(unittest.TestCase):
         exp = Experiment()
         self.assertEqual(exp.condition, [])
 
-    
+    def test_add_condition(self):
+        exp = Experiment()
+        sdt =  SignalDetection(40, 10, 20, 30)
+        exp.add_condition(sdt, "Condition A")
+        self.assertEqual(exp.condition[0][1], "Condition A")
 
 
 
