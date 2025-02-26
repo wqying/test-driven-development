6ec6487ed440f19a08771934cec447845cb9eec7
wqying <qywong@uci.edu>
2025-02-26 16:23:31 +0000

    Created and tested sorted_roc_points

--- src/Experiment.py
+++ src/Experiment.py
@@ -15,9 +15,25 @@ class Experiment:
         '''
         self.condition.append((sdt_obj, label))  # made this into a tuple so that the object and label is connected
 
-    def sorted_roc_points(self) -> tuple[list[float], list[float]]:
+    def sorted_roc_points(self) -> tuple[list[float], list[float]]:  # This method was created with the help of ChatGPT
         '''
         Return sorted false alarm rates and hit rates for plotting the ROC curve
         '''
-        pass
+        # If no conditions are present, raise ValueError
+        if len(self.condition) == 0:
+            raise ValueError
+        
+        false_alarm_rates = []
+        # Since self.condition is a list, we can iterate through the elements inside
+        # Note that the elements inside self.condition are tuples, so we need to iterate through both of them
+        for sdt_obj, _ in self.condition:  # we ignore the labels, so we use '_'
+            false_alarm_rates.append(sdt_obj.false_alarm_rate())  # Remember that false_alarm_rate() is from SignalDetectionTheory
+        # We can also use list comprehension:
+        # false_alarm_rates = [sdt_obj.false_alarm_rate() for sdt_obj, _ in self.condition]
+        first_element = sorted(false_alarm_rates)
+
+
+
+
+
          
--- src/TestExperiment.py
+++ src/TestExperiment.py
@@ -12,8 +12,17 @@ class TestExperiment(unittest.TestCase):
     def test_add_condition(self):
         exp = Experiment()
         sdt =  SignalDetection(40, 10, 20, 30)
+        sdt2 = SignalDetection(1, 2, 3, 4)
         exp.add_condition(sdt, "Condition A")
+        exp.add_condition(sdt2, "Condition B")
         self.assertEqual(exp.condition[0][1], "Condition A")
+        self.assertEqual(exp.condition[1][1], "Condition B")
+
+    def test_sorted_roc_points(self):
+        exp = Experiment()
+        with self.assertRaises(ValueError):
+            exp.sorted_roc_points()
+
 
 
 
