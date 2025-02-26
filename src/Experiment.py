fead2953d770e2faa968b34aa80cdf5d9a82d933
wqying <qywong@uci.edu>
2025-02-26 14:06:07 +0000

    Intialized Experiment class

--- src/Experiment.py
+++ src/Experiment.py
@@ -1,1 +1,17 @@
 # Experiment class
+
+
+class Experiment:
+    def __init__(self):
+        '''
+        Initializes an empty list to store SDT objects and their corresponding labels
+        '''
+        self.condition = []
+
+    def add_condition(self, sdt_obj: SignalDetection, label: str = None) -> None:
+        '''
+        Adds a SignalDetection object and an optional label describing the condition to the experiment
+        '''
+        self.condition.append((sdt_obj, label))
+
+    
